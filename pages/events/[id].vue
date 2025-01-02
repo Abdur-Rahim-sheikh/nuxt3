3a7fa0a57b46b492a97344748b0fb7c1a715e314
Abdur-Rahim-sheikh <abi.rahim.sheikh@gmail.com>
2025-01-02 15:34:06 +0600

    query parameter

---
+++ pages/events/[id].vue
@@ -0,0 +1,21 @@
+<!-- <script setup>
+    const route = useNuxtApp();
+</script>
+<template>
+    <div>event id: {{ id }}</div>
+</template> -->
+
+<!-- to access the [id] variable as in file name we can use useRoute() -->
+<script setup>
+    const route = useRoute();
+    const id = ref(route.params.id);
+    console.log("id value", id.value);
+</script>
+<template>
+    <div>event id: {{ id }}</div>
+</template>
+
+<!-- Or we can directly catch value using $ sign -->
+<!-- <template>
+    <div>event id: {{ $route.params.id }}</div>
+</template> -->
