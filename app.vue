eb73c1dd0644ea21622a02b53a340199c6aad49e
Abdur-Rahim-sheikh <abi.rahim.sheikh@gmail.com>
2025-01-02 15:38:52 +0600

    nuxtlink

--- app.vue
+++ app.vue
@@ -1,6 +1,14 @@
 <template>
   <h1>
     <NuxtLayout>
+      <header>
+        <ul>
+          <li>
+            <NuxtLink to="/"> Home</NuxtLink>
+            <NuxtLink to="/events">Events</NuxtLink>
+          </li>
+        </ul>
+      </header>
       <NuxtPage />
     </NuxtLayout>
   </h1>
