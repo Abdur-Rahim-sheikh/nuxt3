0c019e51cf87573c216d4c73895e2525c1bceb7a
Abdur-Rahim-sheikh <abi.rahim.sheikh@gmail.com>
2025-01-03 14:37:36 +0600

    composable

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,10 @@
+<script>
+    // const sayHello = () => console.log('Hello from the index page')
+    // sayHello();
+    const {sayHello} = useUtils();
+    sayHello();
+</script>
+
 <template>
     <div class="main">
         <h1>Index Page</h1>
