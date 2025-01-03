dd56ab8979d0b1a64352997d0a356a2b51ae6f34
Abdur-Rahim-sheikh <abi.rahim.sheikh@gmail.com>
2025-01-03 13:13:04 +0600

    icon as component

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,8 @@
 <template>
-    <div>Index Page</div>
+    <div class="main">
+        <h1>Index Page</h1>
+        <!-- <img src="/2.jpeg" alt="kindle book" /> -->
+        <IconsBell />
+
+    </div>
 </template>
