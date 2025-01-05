065340db07ecfb1030dd1478bfc9749a855ea82d
Abdur-Rahim-sheikh <abi.rahim.sheikh@gmail.com>
2025-01-05 11:51:20 +0600

    pages

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h2>Home</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit possimus qui aspernatur consectetur ut. Eum vero aut laudantium saepe doloribus?</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit possimus qui aspernatur consectetur ut. Eum vero aut laudantium saepe doloribus?</p>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+h2{
+    margin-bottom: 20px;
+    font-size: 36px;
+}
+p{
+    margin: 20px 0;
+}
+</style>
