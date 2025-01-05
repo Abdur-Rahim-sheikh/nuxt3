d3424c10517c97afb488341b5741fe6e4950d23f
Abdur-Rahim-sheikh <abi.rahim.sheikh@gmail.com>
2025-01-05 15:07:40 +0600

    use fetch

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -1,12 +1,16 @@
 <template>
   <div>
-    <p>Product details for {{ id }}</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam rem eligendi odit.</p>
+    <p>{{ product.title }}</p>
+    <p>{{ product.price }}</p>
+    <p>{{ product.id }}</p>
   </div>
 </template>
 
 <script lang="ts" setup>
 const { id } = useRoute().params;
+const uri = `https://fakestoreapi.com/products/${id}`;
+// fetch product details
+const { data: product } = await useFetch(uri);
 definePageMeta({
   layout: 'products',
 });
