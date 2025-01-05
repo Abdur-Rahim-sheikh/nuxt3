7b91b975e6effa9ceb3b682372a3934bd71ea871
Abdur-Rahim-sheikh <abi.rahim.sheikh@gmail.com>
2025-01-05 12:01:51 +0600

    params

---
+++ pages/products/[id].vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <p>Product details for {{ id }}</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam rem eligendi odit.</p>
+  </div>
+</template>
+
+<script lang="ts" setup>
+    const { id } = useRoute().params;
+</script>
+
+<style>
+
+</style>
