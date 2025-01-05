5843d65d3e4c8a5554f0434a84b2d86363ca38a5
Abdur-Rahim-sheikh <abi.rahim.sheikh@gmail.com>
2025-01-05 14:34:07 +0600

    tailwind

--- layouts/products.vue
+++ layouts/products.vue
@@ -1,18 +1,18 @@
 <template>
     <div>
-        <header>
-            <nav>
-                <NuxtLink to="/">Nuxt Ninja Merch</NuxtLink>
+        <header class="shadow-sm bg-white">
+            <nav class="container mx-auto p-4">
+                <NuxtLink to="/" class="font-bold">Nuxt Ninja Merch</NuxtLink>
             </nav>
 
         </header>
         <!-- output the page content -->
-        <div>
+        <div class="container mx-auto p-4">
             <slot />
         </div>
 
-        <footer>
-            <ul>
+        <footer class="container mx-auto p-4 flex justify-between border-t-2">
+            <ul class="flex gap-4">
                 <li>
                     <NuxtLink to="/">Home</NuxtLink>
                 </li>
