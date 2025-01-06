76696a753ca63e34f3c6476c893a92afecdd72ea
Abdur-Rahim-sheikh <abi.rahim.sheikh@gmail.com>
2025-01-06 13:15:20 +0600

    post request to server

--- pages/about.vue
+++ pages/about.vue
@@ -1,21 +1,28 @@
 <template>
-    <div>
-      <h2>About</h2>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit possimus qui aspernatur consectetur ut. Eum vero aut laudantium saepe doloribus?</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit possimus qui aspernatur consectetur ut. Eum vero aut laudantium saepe doloribus?</p>
-    </div>
-  </template>
-  
-  <script lang="ts" setup>
-  
-  </script>
-  
-  <style>
-  h2{
-      margin-bottom: 20px;
-      font-size: 36px;
-  }
-  p{
-      margin: 20px 0;
-  }
-  </style>
+  <div>
+    <h2>About</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit possimus qui aspernatur consectetur ut. Eum vero
+      aut laudantium saepe doloribus?</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit possimus qui aspernatur consectetur ut. Eum vero
+      aut laudantium saepe doloribus?</p>
+    <p> From server api: {{ data }}</p>
+  </div>
+</template>
+
+<script setup>
+const { data } = await useFetch('/api/ninja?name=creator', {
+  method: 'POST',
+  body: { age: 30 }
+})
+</script>
+
+<style>
+h2 {
+  margin-bottom: 20px;
+  font-size: 36px;
+}
+
+p {
+  margin: 20px 0;
+}
+</style>
