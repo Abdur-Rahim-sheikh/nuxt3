b544f99ded1d411d652df954420a3370ac193162
Abdur-Rahim-sheikh <abi.rahim.sheikh@gmail.com>
2025-01-06 14:16:47 +0600

    update

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,7 @@
     <div>
         <header class="shadow-sm bg-white">
             <nav class="container mx-auto p-4 flex justify-between">
-                <NuxtLink to="/" class="font-bold">Nuxt Ninja</NuxtLink>
+                <NuxtLink to="/" class="font-bold">Abir web Page</NuxtLink>
                 <ul class="flex gap-4">
                     <li>
                         <NuxtLink to="/">Home</NuxtLink>
